735c28a4222f1bec01ec2f7a1e03ebadfc284cab
tony-armstrong <tony321armstrong@gmail.com>
2023-10-10 20:11:00 +0800

    feat: add task PolygonZkEVM:parseLog, PolygonZkEVM:info

--- helper.sh
+++ helper.sh
@@ -14,21 +14,28 @@ run() {
     # Proof of Efficiency
     # POE
     # PolygonZkEVM
-    forge flatten --hardhat contracts/PolygonZkEVM.sol >tmp.sol
-    return
+    # forge flatten --hardhat contracts/PolygonZkEVM.sol >tmp.sol
+    # return
 
     # npx hardhat --help
     # time npx hardhat --network polygonL1net scanEOAAndContract
     # time npx hardhat --network polygonL1net debug
-    # time npx hardhat --network polygonL1net PolygonZkEVM
-
+    txhashs='0x765730c24d1b99ee43c89f691f93c13cf9dfeac68280b2698e9cc4e349f34848,0x6b3dce636a755a7eb74b5b76ebee5262203bad0437f805ec5e22fa692391f239'
+    npx hardhat --network polygonL1net PolygonZkEVM:parseLog \
+        --addr 0x610178dA211FEF7D417bC0e6FeD39F05609AD788 \
+        --txhash $txhashs | jq .
+        # --txhash $txhashs
+    return
+    time npx hardhat --network polygonL1net PolygonZkEVM \
+        --addr 0x610178dA211FEF7D417bC0e6FeD39F05609AD788
+    return
     time npx hardhat --network polygonL1net findTxOfAAddr \
         --addr 0x610178dA211FEF7D417bC0e6FeD39F05609AD788
-        # --height 10000
+    # --height 10000
     return
 }
 
-verify(){
+verify() {
     ADDR_POE=0x610178dA211FEF7D417bC0e6FeD39F05609AD788
     npx hardhat verify \
         --network polygonL1net \
